4d347cb3792aba22ce4585965e4c35ce9f401391
minseong <kdsvip5@naver.com>
2022-06-25 12:57:53 +0900

    Initial commit

---
+++ src/main/java/com/example/minlog/MinlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.minlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinlogApplication.class, args);
+	}
+
+}
